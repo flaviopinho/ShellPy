c8cba1d2ef16f5129193e658ad4b3a46887c24ca
Flávio Augusto Xavier Carneiro Pinho <flavio.axcp@gmail.com>
2026-02-09 16:47:43 -0300

    Arrumando inits 4

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     python_requires="==3.12.*",
     install_requires=install_requires,
     classifiers=[
-        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.12",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
